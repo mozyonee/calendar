ebc2076d5615b684e9f84716966b9f34548f3265
Vadym Abakumov <vadym.abakumov@gmail.com>
2026-03-09 19:22:03 +0100

    fix: cleanup next config

--- apps/client/next.config.ts
+++ apps/client/next.config.ts
@@ -1,20 +1,5 @@
 import type { NextConfig } from 'next';
-import path from 'path';
 
-type OutputMode = 'standalone' | 'export' | undefined;
-const outputMode = (process.env.NEXT_OUTPUT as OutputMode) ?? 'standalone';
-
-const nextConfig: NextConfig = {
-	output: outputMode,
-	outputFileTracingRoot: path.join(__dirname, '../../'),
-	images: {
-		remotePatterns: [
-			{ hostname: 'localhost' },
-			...(process.env.NEXT_PUBLIC_SERVER_URL
-				? [{ hostname: new URL(process.env.NEXT_PUBLIC_SERVER_URL).hostname }]
-				: []),
-		],
-	},
-};
+const nextConfig: NextConfig = { };
 
 export default nextConfig;
